10b7734688b4e26c4ca740e504f56eac522336c5
Backsta3 <darkstarfar35@gmail.com>
2024-02-07 17:16:38 +0600

    CONFIG: Firebase

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Firebase configuration
@@ -11,12 +11,15 @@ const firebaseConfig = {
     storageBucket: "fake-store-81.appspot.com",
     messagingSenderId: "647505369200",
     appId: "1:647505369200:web:abedf51e186a0a57ec3cb5"
-  };
-
+};
 
 // Initializing Firebase
 const APP = initializeApp(firebaseConfig);
-export const FIREBASE_APP = initializeAuth(APP , {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
-export const FIREBASE_AUTH = getAuth(APP);
+
+// Firebase Authentication with persistence
+export const FIREBASE_AUTH = initializeAuth(APP, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  });
+
+// Initializing Firestore
+export const FIREBASE_DB = getFirestore(APP);
